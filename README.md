80774a41cb843e0925cc0d9aa8e1f4cbbbcc47c5
Jose Alanya <jose.alanya@gmail.com>
2017-04-26 09:04:01 -0500

    FEAT: Components - Composing Components

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react-fundamentals-pluralsight
+
+Populating props
+
+e.g: https://codepen.io/jalanya/pen/WjoBXR
+
+Composing Components
+
+e.g: https://codepen.io/jalanya/pen/xdRodx
--- js/app.js
+++ js/app.js
@@ -3,9 +3,23 @@
 
   var Quiz = React.createClass({
     render: function() {
-      return <div>Welcome to Quiz app created by { this.props.creator }.</div>;
+      return <div>
+               <span>Welcome to Quiz app created by { this.props.creator }.</span>
+               <div>
+                { this.props.books.map(function(b){
+                    return <Book title={b} />;
+                })}
+               </div>
+             </div>;
     }
   });
 
-  ReactDOM.render(<Quiz creator={ "Jose Alanya"}/>, document.getElementById('container'));
+  var Book = React.createClass({
+    render: function() {
+      return <div><h4>{ this.props.title }</h4></div>;
+    }
+  });
+
+  ReactDOM.render(<Quiz creator={ "Jose Alanya"}
+                   books={ ['The Lord of The Rings', 'The Iliad']}/>, document.getElementById('container'));
 })();
