9e80caf7733fa7f5bf4fc8b14d4d4409d7d2cf98
Jose Alanya <jose.alanya@gmail.com>
2017-04-26 08:09:51 -0500

    FEAT: Components - The Author Quiz
    - Bootstrapping the Author Quiz
    
    - Rendering Components

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  var Quiz = React.createClass({
+    render: function() {
+      return <div>Welcome to Quiz app.</div>;
+    }
+  });
+
+  ReactDOM.render(<Quiz />, document.getElementById('container'));
+})();
