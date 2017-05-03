e12d1365ae995fc4c20912f7380acfda8130729b
Jose Alanya <jose.alanya@gmail.com>
2017-05-03 06:40:21 -0700

    FEAT: JSX - The Author Quiz

--- js/app.js
+++ js/app.js
@@ -3,15 +3,23 @@
 
   var Quiz = React.createClass({
     propTypes: {
-       books: React.PropTypes.array.isRequired
+       data: React.PropTypes.array.isRequired
+    },
+    getInitialState: function() {
+      return this.props.data.selectGame();
     },
     render: function() {
       return <div>
-               <span>Welcome to Quiz app created by { this.props.creator }.</span>
-               <div>
-                { this.props.books.map(function(b){
-                    return <Book title={b} />;
-                })}
+               <div className="row">
+                  <div className="col-md-4">
+                      <img src={this.state.author.imageUrl} className="authorimage"/>
+                  </div>
+                  <div className="col-md-7">
+                    { this.state.books.map(function(b) {
+                      return <Book title={b} />;
+                    }, this)}
+                  </div>
+                  <div className="col-md-1"></div>
                </div>
              </div>;
     }
@@ -22,10 +30,64 @@
       title: React.PropTypes.string.isRequired
     },
     render: function() {
-      return <div><h4>{ this.props.title }</h4></div>;
+      return <div className="answer"><h4>{ this.props.title }</h4></div>;
     }
   });
+  var data = [
+      {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        books: ['The Adventures of Huckleberry Finn']
+      },
+      {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.png',
+        books: ['Heart of Darkness']
+      },
+      {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+      },
+      {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining','IT']
+      },
+      {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesdickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+      },
+      {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/williamshakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+      }
+  ];
 
-  ReactDOM.render(<Quiz creator={ "Jose Alanya"}
-                   books={ ['The Lord of The Rings', 'The Iliad']}/>, document.getElementById('container'));
+  data.selectGame = function () {
+    var books = _.shuffle(this.reduce(function(p, c, i) {
+        return p.concat(c.books);
+    }, [])).slice(0, 4);
+
+    var answer = books[_.random(books.length - 1)];
+
+    return {
+      books: books,
+      author: _.find(this, function(author) {
+        return author.books.some(function (title) {
+          return title === answer;
+        });
+      })
+    }
+  };
+
+  ReactDOM.render(<Quiz data={ data }/>, document.getElementById('app'));
 })();
