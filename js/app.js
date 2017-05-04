037ef9e8d3fa3f19f10c06b2815123242c932c89
Jose Alanya <jose.alanya@gmail.com>
2017-05-04 08:51:08 -0700

    FEAT: Events - The Author Quiz

--- js/app.js
+++ js/app.js
@@ -6,7 +6,20 @@
        data: React.PropTypes.array.isRequired
     },
     getInitialState: function() {
-      return this.props.data.selectGame();
+      return _.extend({
+        bgClass: 'neutral',
+        showContinue: false,
+      }, this.props.data.selectGame());
+    },
+    handleBookSelected: function(title) {
+      var isCorrect = this.state.checkAnswer(title);
+      this.setState({
+        bgClass: isCorrect ? 'pass' : 'fail',
+        showContinue: isCorrect
+      });
+    },
+    handleContinue: function() {
+      this.setState(this.getInitialState());
     },
     render: function() {
       return <div>
@@ -16,11 +29,18 @@
                   </div>
                   <div className="col-md-7">
                     { this.state.books.map(function(b) {
-                      return <Book title={b} />;
+                      return <Book onBookSelected={this.handleBookSelected} title={b} />;
                     }, this)}
                   </div>
-                  <div className="col-md-1"></div>
+                  <div className={ "col-md-1 " + this.state.bgClass } ></div>
                </div>
+               {this.state.showContinue ? (
+                <div className="row">
+                  <div className="col-md-12">
+                    <input onClick={this.handleContinue} type="button" className="btn btn-primary btn-lg pull-right" value="Continue" />
+                  </div>
+                </div>): <span/>
+              }
              </div>;
     }
   });
@@ -29,8 +49,13 @@
     propTypes: {
       title: React.PropTypes.string.isRequired
     },
+    handleClick: function() {
+      this.props.onBookSelected(this.props.title);
+    },
     render: function() {
-      return <div className="answer"><h4>{ this.props.title }</h4></div>;
+      return <div onClick={this.handleClick} className="answer">
+                  <h4>{ this.props.title }</h4>
+            </div>;
     }
   });
   var data = [
@@ -85,7 +110,12 @@
         return author.books.some(function (title) {
           return title === answer;
         });
-      })
+      }),
+      checkAnswer: function(title) {
+        return this.author.books.some(function (t) {
+            return t === title;
+        });
+      }
     }
   };
 
